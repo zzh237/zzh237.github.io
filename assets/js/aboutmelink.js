6fb4bb4b1226f0632abcdf0759ab0907eb2df743
zzh237 <zhizhang5763@gmail.com>
2024-01-21 12:22:50 -0800

    new

--- assets/js/aboutmelink.js
+++ assets/js/aboutmelink.js
@@ -1,22 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // 监听所有 '.about-me-link' 的点击事件
   document.querySelectorAll('.about-me-link').forEach(function(link) {
     link.addEventListener('click', function(event) {
-      // 取消默认的链接行为
       event.preventDefault();
-      
-      // 获取要导航到的链接的URL
-      var targetUrl = link.getAttribute('href');
 
-      // 在侧边栏中找到对应的链接
+      var targetUrl = link.getAttribute('href');
       var sidebarLink = document.querySelector('#sidebar-right .toc-link[href="' + targetUrl + '"]');
+
+      var sidebarLinks = document.querySelectorAll('#sidebar-right .toc-link');
+      var publicationCategory = 'publications'; // This should match your publications category exactly.
+
+      // Hide all links in the sidebar that are not publications
+      sidebarLinks.forEach(function(sidebarLink) {
+        var isPublication = sidebarLink.dataset.tags.split(' ').includes(publicationCategory);
+        sidebarLink.style.display = isPublication ? 'block' : 'none';
+      });
+
+      // Click the sidebar link if it exists
       if (sidebarLink) {
-        // 如果找到了侧边栏中的链接，模拟点击它
         sidebarLink.click();
       } else {
-        // 如果没有找到，可能需要重定向到目标URL
         window.location.href = targetUrl;
       }
     });
   });
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   // 监听所有 '.about-me-link' 的点击事件
+//   document.querySelectorAll('.about-me-link').forEach(function(link) {
+//     link.addEventListener('click', function(event) {
+//       // 取消默认的链接行为
+//       event.preventDefault();
+      
+//       // 获取要导航到的链接的URL
+//       var targetUrl = link.getAttribute('href');
+
+//       // 在侧边栏中找到对应的链接
+//       var sidebarLink = document.querySelector('#sidebar-right .toc-link[href="' + targetUrl + '"]');
+//       if (sidebarLink) {
+//         // 如果找到了侧边栏中的链接，模拟点击它
+//         sidebarLink.click();
+//       } else {
+//         // 如果没有找到，可能需要重定向到目标URL
+//         window.location.href = targetUrl;
+//       }
+//     });
+//   });
+// });
