c918859db06f4fa8313219f99d79feef69cd7f98
zzh237 <zhizhang5763@gmail.com>
2024-01-21 11:41:22 -0800

    new

--- assets/js/aboutmelink.js
+++ assets/js/aboutmelink.js
@@ -1,52 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // 获取所有侧边栏链接
-  var sidebarLinks = document.querySelectorAll('#sidebar-right .toc-link');
-  var sidebarCategories = document.querySelectorAll('#sidebar-tags .sidebar-tag');
-
-  // 为每个 "About Me" 链接添加点击事件监听器
+  // 监听所有 '.about-me-link' 的点击事件
   document.querySelectorAll('.about-me-link').forEach(function(link) {
     link.addEventListener('click', function(event) {
-      // 获取点击的链接对应的文章标题
-      var clickedPostTitle = link.innerText;
+      // 取消默认的链接行为
+      event.preventDefault();
+      
+      // 获取要导航到的链接的URL
+      var targetUrl = link.getAttribute('href');
 
-      // 激活侧边栏的 "Publications" 标签
-      sidebarCategories.forEach(function(category) {
-        category.classList.remove('active');
-        if (category.dataset.filter === 'publication') {
-          category.classList.add('active');
-        }
-      });
-
-      // 显示 "Publications" 分类下的文章
-      var publicationPosts = [];
-      sidebarLinks.forEach(function(sidebarLink) {
-        var postCategories = sidebarLink.dataset.tags.split(' ');
-        if (postCategories.includes('publication')) {
-          publicationPosts.push(sidebarLink);
-        }
-      });
-
-      // 隐藏所有侧边栏链接，只显示 "Publications" 文章
-      sidebarLinks.forEach(function(link) {
-        link.style.display = 'none';
-      });
-      publicationPosts.forEach(function(link) {
-        link.style.display = 'block';
-      });
-
-      // 尝试在侧边栏中找到对应的文章链接
-      var found = false;
-      publicationPosts.forEach(function(sidebarLink) {
-        if (sidebarLink.innerText.trim() === clickedPostTitle) {
-          found = true;
-          window.location.href = sidebarLink.href; // 直接跳转到相应页面
-        }
-      });
-
-      // 如果没有找到对应的文章链接，则可能需要考虑重定向或其他行为
-      if (!found) {
-        console.error('No matching post found in the sidebar for:', clickedPostTitle);
-        // 可以在这里添加重定向或错误处理
+      // 在侧边栏中找到对应的链接
+      var sidebarLink = document.querySelector('#sidebar-right .toc-link[href="' + targetUrl + '"]');
+      if (sidebarLink) {
+        // 如果找到了侧边栏中的链接，模拟点击它
+        sidebarLink.click();
+      } else {
+        // 如果没有找到，可能需要重定向到目标URL
+        window.location.href = targetUrl;
       }
     });
   });
